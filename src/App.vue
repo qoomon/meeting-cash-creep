54510975577099b67c53038c5efdccfbdf4eb4b5
Bengt Brodersen <bengt.brodersen@gmail.com>
2021-09-29 11:36:30 +0200

    feat: add multiple cost entries

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <n-config-provider :theme="darkTheme">
+    <img alt="Vue logo" src="./assets/logo.png">
 
-  <p>
-    <Flip :value="counter" 
-      style="margin: auto; font-size: 10vw;"/>
+    <Flip :value="totalCosts" style="margin: auto; font-size: 10vw;"/>
+    <!-- <Roller :value="counter" style="margin: auto; font-size: 10vw;"/> -->
+    
     <br>
-    <Roller :value="counter"
-      style="margin: auto; font-size: 10vw;"/>
-  </p>
+
+    <n-dynamic-input
+    v-model:value="costsArray"
+    :on-create="addCost"
+    #="{ value }"
+    style="margin: auto; width: 40em; max-width: 96vw;"
+    >
+      <div style="width: 100%; display: flex; align-items: center;">
+        <n-checkbox v-model:checked="value.active" style="margin-right: 12px;" />
+        <n-input-number
+          v-model:value="value.value"
+          style="margin-right: 12px; width: 160px;"
+        />
+        <n-input v-model:value="value.description" type="text" />
+      </div>
+    </n-dynamic-input>
+  </n-config-provider>
 </template>
 
 <script>
+import { 
+  NConfigProvider, darkTheme, 
+  NDynamicInput, NInput, NInputNumber, NCheckbox, NSpace
+} from 'naive-ui'
 import Flip from "./components/Flip";
 import Roller from "./components/Roller";
-
+  
 export default {
   name: 'App',
   components: {
+    NConfigProvider, NDynamicInput, NInput, NInputNumber, NCheckbox, NSpace,
     Flip,
     Roller
   },
   data() {
     return {
-      counter: 0
-    };
-  },
-  methods: {
-    increase() {
-      this.counter += 1013
+      darkTheme,
+      totalCosts: 0,
+      totalCostsUpdateIterval: null,
+      costsArray: [
+        {
+          active: true,
+          value: 99,
+          description: 'Coding Dude'
+        }
+      ]
     }
   },
-  mounted() {
-    setInterval(this.increase,1000)
+  computed: {
+    costsPerSecond(){
+      return this.costsArray
+        .filter(cost => cost.active && cost.value)
+        .map(cost => cost.value)
+        .reduce((a, b) => a + b, 0)
+    }
+  },
+  methods: {
+    costTick() {
+      this.totalCosts += this.costsPerSecond
+    },
+    addCost () {
+        return {}
+      }
+  },
+  created() {
+    this.totalCostsUpdateIterval = setInterval(this.costTick, 1000)
+  },
+  beforeDestroy() {
+    clearInterval(this.totalCostsUpdateIterval)
   }
 }
 </script>
 
 <style>
 body {
+  position: relative;
+  box-sizing: border-box;
   width: 100vw;
   min-height: 100vh;
-  position: relative;
-  padding: 1.25rem 2.5rem 1.25rem 1.5rem;
+  padding: 0;
   background-color: #40414a;
   overflow-y: auto;
 }
